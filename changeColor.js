772b2dfb67b5649483a0e01a61fa53a952e67f01
Parkayun <iamparkayun@gmail.com>
2016-02-09 23:05:54 +0900

    Apply color scheme everytime

--- changeColor.js
+++ changeColor.js
@@ -1,9 +1,19 @@
-//$(document).ready(function() {
-    var colors = [ '#1e6823', '#44a340', '#8cc665', '#d6e685', '#eee' ];
-    var converts = [ '#ff5675', '#ff6a89', '#ff88a7', '#ff9cbb', '#ffb0cf' ];
+var colors = [ '#1e6823', '#44a340', '#8cc665', '#d6e685', '#eee' ];
+var converts = [ '#ff5675', '#ff6a89', '#ff88a7', '#ff9cbb', '#ffb0cf' ];
 
-    for (var i = 0; i < colors.length; i += 1) {
-        $('.day[fill='+colors[i] + ']').attr('fill', converts[i]);
-        $('ul.legend li[style="background-color: '+colors[i]+'"]').css('background-color', converts[i]);
-    }
-//});
+var updateColors = function () {
+  for (var i = 0; i < colors.length; i += 1) {
+      $('.day[fill='+colors[i] + ']').attr('fill', converts[i]);
+      $('ul.legend li[style="background-color: '+colors[i]+'"]').css('background-color', converts[i]);
+  }
+}
+
+$('#js-pjax-container').bind("DOMSubtreeModified DOMNodeRemoved DOMElementNameChanged", function() {
+  if ($('.contributions-tab').length == 1) {
+      updateColors();
+  }
+});
+
+// Initialize
+updateColors();
+
